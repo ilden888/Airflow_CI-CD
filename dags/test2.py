4ce9c98423142f53fec9207b3dc2aa85b7cef882
Vadim Demchenko <v.demchenko@revizto.com>
2024-03-19 00:15:31 +0400

    Fix scp and new test files

--- dags/test2.py
+++ dags/test2.py
@@ -1,21 +1,51 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+
+import pendulum
+
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators import MultiplyBy5Operator
 
-def print_hello():
- return 'Hello Wolrd'
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
 
-dag = DAG('hello_world', description='Hello world example', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
 
-dummy_operator = DummyOperator(task_id='dummy_task', retries = 3, dag=dag)
+args = {
+    'owner': 'k0rsakov',
+    'start_date': datetime(2023, 3, 10, tzinfo=pendulum.timezone('Europe/Moscow')),
+    'catchup': True,
+    'retries': 3,
+    'retry_delay': timedelta(hours=1),
+    'max_active_runs': 1
+}
 
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 
-multiplyby5_operator = MultiplyBy5Operator(my_operator_param='my_operator_param',
-                                task_id='multiplyby5_task', dag=dag)
+def print_something() -> None:
+    """
+    Печатает текст.
 
-dummy_operator >> hello_operator
+    :return: `None`
+    """
+    print('something')
 
-dummy_operator >> multiplyby5_operator
+with DAG(
+        dag_id='template',
+        schedule_interval='10 0 * * *',
+        default_args=args,
+        tags=['template', 'test'],
+        description='',
+        concurrency=1
+) as dag:
+
+    start = EmptyOperator(
+        task_id='start',
+    )
+
+    print_something = PythonOperator(
+        task_id='print_something',
+        python_callable=print_something,
+    )
+
+    end = EmptyOperator(
+        task_id='end',
+    )
+
+    start >> print_something >> end
